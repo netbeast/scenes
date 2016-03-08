d4f78b2b8c889d4da424bd522d372d8c808cfb23
pablopi <pablopizarroaguilar@gmail.com>
2016-03-08 18:10:18 +0100

    Morning scene on way

--- components/picker.jsx
+++ components/picker.jsx
@@ -3,23 +3,39 @@ import React from 'react'
 import netbeast from 'netbeast'
 
 export default class Picker extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      color: '#fff',
-      power: false
-    }
+
+  handleMorning () {
+    // 
+    // netbeast('music').set({volume: 15, track: __dirname + '/media/morningsong.mp3'})
+    // .then(function (data) {
+    //   console.log(data)
+    // }).catch(function (err) {
+    //   console.log('ERR')
+    //   console.log(err)
+    // })
+    //
+    var i = 0
+    var timer = setInterval(function () {
+      if (i >= 0 && i <= 25) {
+        netbeast('lights').set({color: {r: 10 * i, g: 10 * i, b: 10 * i}})
+        i++
+      } else clearInterval(timer)
+    }, 400)
+
   }
 
-  handleClick () {
+  handleFilm () {
+  }
+
+  handleParty () {
   }
 
   render () {
     return (
       <div>
-        <button onClick={ this.handleClick.bind(this, 'good-morning') }>Good Morning</button>
-        <button onClick={ this.handleClick.bind(this, 'film') }>Watching a Film</button>
-        <button onClick={ this.handleClick.bind(this, 'party') }>Party</button>
+        <button onClick={ this.handleMorning.bind(this) }>Good Morning</button>
+        <button onClick={ this.handleFilm.bind(this) }>Watching a Film</button>
+        <button onClick={ this.handleParty.bind(this) }>Party</button>
       </div>
     )
   }
